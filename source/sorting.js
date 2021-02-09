6e906e8ea64c197118c77e4e5d73ce5b4e1e26ea
Anastasia Morozova <morozovaanastasiaa2001@gmail.com>
2021-02-10 00:40:41 +0300

    function edited

--- source/sorting.js
+++ source/sorting.js
@@ -3,13 +3,14 @@
 // сортируем по каждому свойству из массива свойств в обратном порядке
 // т.е. начиная с меньшего приоритета, передаем ф-цию сравнения в sort()
 function sorting(initial, fields){
+    let copy = Object.assign([], initial);
     fields.reverse().forEach((item) => {
-        initial.sort((prev, next) => {
+        copy.sort((prev, next) => {
             if (prev[item] < next[item]) return -1;
             if (prev[item] > next[item]) return 1;
         })
     }
     )
-    return initial;
+    return copy;
 }
 
